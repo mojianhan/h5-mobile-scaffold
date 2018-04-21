f4ea172583bbe82fd3e1265c519710ee5e9c78b6
cntanglijun <869058216@qq.com>
2018-04-22 03:56:59 +0800

    引入 requirejs 加载器

---
+++ src/scripts/config.js
@@ -0,0 +1,21 @@
+
+// The config file of requirejs
+// More: http://requirejs.org/docs/api.html#config
+
+require.config({
+  baseUrl: './scripts',
+  paths: {
+
+    // http://jquery.com/
+    jquery: '//cdn.bootcss.com/jquery/1.12.1/jquery.min',
+
+    // https://lodash.com/
+    lodash: '//cdn.bootcss.com/lodash.js/4.16.4/lodash.min',
+
+    // http://bluebirdjs.com/docs/getting-started.html
+    bluebird: '//cdn.bootcss.com/bluebird/3.3.2/bluebird.min',
+
+    // https://github.com/js-cookie/js-cookie
+    cookie: '//cdn.bootcss.com/js-cookie/2.1.0/js.cookie.min'
+  }
+});
